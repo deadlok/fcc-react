6c7ff6eb6bf891f67a55ce815e2ec49271bcda76
Lok <lok@pg95.com>
2024-02-13 13:58:31 +0000

    finished markdown previewer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Markdown from './markdown/Markdown';
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Redirect,
+ } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode>    
+  <Router>
+	  <Routes>
+		  <Route path="/" element={<div><h1>Home</h1></div>}/>
+		  <Route path="/markdown" element={<Markdown/>}/>
+	  </Routes>
+</Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
---
+++ src/markdown/Markdown.js
@@ -0,0 +1,89 @@
+import './Markdown.css';
+import React from 'react'
+import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
+
+
+class Markdown extends React.Component {
+  constructor(props){
+    super(props);
+
+    let rawStr = 
+`
+# Welcome to my React Markdown Previewer!
+    
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`
+
+    //console.log(marked.parse(rawStr))
+
+    this.state = {
+      input: rawStr,
+      markdown: marked.parse(rawStr,{breaks:true})
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event){
+    //console.log(event.target.value);
+
+    this.setState({input: event.target.value,
+                  markdown: marked.parse(event.target.value,{breaks:true})});
+
+    //console.log(this.state);
+  }
+  render () {
+    return ( 
+    <div className="App">
+      <div className="editorWrap">
+      <textarea id="editor" rows="20" cols="80" value ={this.state.input} onChange={this.handleChange}>
+      </textarea>
+      </div>
+      <div id="preview" className="previewWrap" dangerouslySetInnerHTML={{__html: this.state.markdown}}>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default Markdown;
